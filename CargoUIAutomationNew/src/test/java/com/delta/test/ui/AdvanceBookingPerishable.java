e1a24de8d47fdcfdf3cf0502a0bc07ad6b733269
BhavaniAkuthota <131790565+BhavaniAkuthota@users.noreply.github.com>
2025-02-04 08:51:28 -0500

    Delete CargoUIAutomationNew directory

--- CargoUIAutomationNew/src/test/java/com/delta/test/ui/AdvanceBookingPerishable.java
+++
@@ -1,8 +0,0 @@
-package com.delta.test.ui;
-
-import com.delta.util.BaseTest;
-
-public class AdvanceBookingPerishable extends BaseTest {
-
-
-}
--- CargoUIAutomationNew/src/test/java/com/delta/test/ui/GuidedBookingMedical.java
+++
@@ -1,4 +0,0 @@
-package com.delta.test.ui;
-
-public class GuidedBookingMedical {
-}
